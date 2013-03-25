75e00d34804aac2ded0f99cbd32b82a795fc8490
Nouman Tayyan <noumantayyab@gmail.com>
2013-03-25 17:21:13 +0500

    changing pagesize to 100

--- main/webapp/modules/core/scripts/views/data-table/data-table-view.js
+++ main/webapp/modules/core/scripts/views/data-table/data-table-view.js
@@ -34,7 +34,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 function DataTableView(div) {
   this._div = div;
 
-  this._pageSize = 10;
+  this._pageSize = 100;
   this._showRecon = true;
   this._collapsedColumnNames = {};
   this._sorting = { criteria: [] };

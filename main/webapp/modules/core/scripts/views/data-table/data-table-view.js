15d2523a81d84cc8d4d131d5edb43c3821c3c0c5
Nouman Tayyan <noumantayyab@gmail.com>
2013-03-25 17:22:53 +0500

    changing pagesize to 100

--- main/webapp/modules/core/scripts/views/data-table/data-table-view.js
+++ main/webapp/modules/core/scripts/views/data-table/data-table-view.js
@@ -182,7 +182,7 @@ DataTableView.prototype._renderPagingControls = function(pageSizeControls, pagin
   }
 
   $('<span>Show: </span>').appendTo(pageSizeControls);
-  var sizes = [ 5, 10, 25, 50 ];
+  var sizes = [ 5, 10, 25, 50, 100 ];
   var renderPageSize = function(index) {
     var pageSize = sizes[index];
     var a = $('<a href="javascript:{}"></a>')
